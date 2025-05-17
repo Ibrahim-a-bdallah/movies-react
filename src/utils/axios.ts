954af5a7ef0601ab574bb15d4f23d1f87c0a6e8d
hemade <hemade@gmail.com>
2025-05-17 15:23:58 +0300

    #

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_KEY = import.meta.env.API_KEY;
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 axios.create({
   baseURL: "https://api.example.com",
   headers: {
