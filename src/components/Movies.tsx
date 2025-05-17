4e89dffaed21dfa63c56bacc6f0b5ef471d03419
hemade <hemade@gmail.com>
2025-05-17 15:14:47 +0300

    #

--- src/components/Movies.tsx
+++ src/components/Movies.tsx
@@ -20,10 +20,7 @@ const getMovies = async (): Promise<SimpleMovieData[]> => {
 const Movies = () => {
   // const { results } = getMovies;
   const { data } = useQuery({
-    queryKey: [
-      process.env.API_KEY ||
-        "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZjlkNTU0MjBkNmJkMmMzNjEyN2E5YTAyNDg2ZTg3YSIsIm5iZiI6MTc0NzQ1MDQ5MC4zNjIsInN1YiI6IjY4MjdmYTdhN2UwN2Q2ZmZlYzllMmE0MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AL5NGCLc19hDqlLp-H7tkOh0bk_2swslsWdDCV9regE",
-    ],
+    queryKey: ["nowPlayingMovies"],
     queryFn: getMovies,
   });
   console.log(data?.map((e) => e));
--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 
+const API_KEY = import.meta.env.API_KEY;
 axios.create({
   baseURL: "https://api.example.com",
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0ZjlkNTU0MjBkNmJkMmMzNjEyN2E5YTAyNDg2ZTg3YSIsIm5iZiI6MTc0NzQ1MDQ5MC4zNjIsInN1YiI6IjY4MjdmYTdhN2UwN2Q2ZmZlYzllMmE0MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AL5NGCLc19hDqlLp-H7tkOh0bk_2swslsWdDCV9regE`,
+    Authorization: `Bearer ${API_KEY}`,
     accept: "application/json",
   },
 });
